97a541d3591c1ddc4f27c1558b8643286f7b7805
LasyaMoravineni <124281083+LasyaMoravineni@users.noreply.github.com>
2023-07-07 15:48:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,16 @@ window.onscroll=()=> {
     menuBtn.classList.remove('bx-x');
     menuBar.classList.remove('active');
 }
+
+ScrollReveal({
+    reset: true,
+    distance: '30px',
+    duration: 1500,
+    delay: 200,
+});
+
+
+ScrollReveal().reveal('.image', { origin:'right' });
+ScrollReveal().reveal('.name_role, .side_icons', { origin:'left' });
+ScrollReveal().reveal('.skills-items, .about-text, .dwnld, .project-items, .urls, .form' ,{ origin:'bottom' });
+ScrollReveal().reveal('.heading', { origin:'top' });
