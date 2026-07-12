b4931e7982d0495111f8f4d7af2e571eb612f6a3
lasyamacs08@gmail.com <lasyamacs08@gmail.com>
2026-07-12 17:14:33 +0530

    Updated portfolio

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 let menuBtn= document.querySelector('#menu-btn');
 let menuBar=document.querySelector(".links");
+const toggleBtn = document.getElementById("toggleProjectsBtn");
+const projectItems = document.querySelector(".project-items");
+const hiddenProjects = document.querySelectorAll(".hidden-project");
+
 
 menuBtn.onclick= ()=>{
     menuBtn.classList.toggle('bx-x');
@@ -12,14 +16,14 @@ window.onscroll=()=> {
 }
 
 ScrollReveal({
-    reset: true,
+    reset: false,
     distance: '30px',
     duration: 1500,
     delay: 200,
 });
 
 var typed= new Typed(".auto-type",{
-    strings : [" Full-stack dev", " AWS Cloud" , " Generative AI"],
+    strings : [" Java Full-stack", " Python", " Generative AI", " AWS Cloud"],
     typeSpeed : 30,
     backSpeed : 30,
     backDelay: 1000, 
@@ -29,12 +33,44 @@ var typed= new Typed(".auto-type",{
 
 
 ScrollReveal().reveal('.image', { origin:'right' });
-ScrollReveal().reveal('.name_role, .side_icons', { origin:'left' });
-ScrollReveal().reveal('.skills-items, .about-text, .dwnld, .project-items, .urls, .form' ,{ origin:'bottom' });
-ScrollReveal().reveal('.heading', { origin:'top' });
+
+ScrollReveal().reveal('.name_role, .side_icons', {
+    origin:'left'
+});
+
+ScrollReveal().reveal('.heading', {
+    origin:'top'
+});
+
+ScrollReveal().reveal('.about-text, .skills-items, .project-1, .certification-card, .experience-card, .urls, .form', {
+    origin:'bottom',
+    interval:120
+});
 
 let btn=document.querySelector(".submit");
 const form=document.getElementById("myForm");
+
+let expanded = false;
+
+toggleBtn.addEventListener("click", () => {
+
+    expanded = !expanded;
+
+    hiddenProjects.forEach(project => {
+
+        if(expanded){
+            project.classList.add("show");
+        }else{
+            project.classList.remove("show");
+        }
+
+    });
+
+    toggleBtn.innerHTML = expanded
+        ? "Show Less ↑"
+        : "View More Projects ↓";
+
+});
 
 // btn.onclick = (event)=>{
 //     event.preventDefault();
